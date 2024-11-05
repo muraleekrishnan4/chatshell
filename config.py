6070fa164ddc70c03bc2cec64a8f741a188e8490
Muraleekrishnan Unnithan <63204975+muraleekrishnan4@users.noreply.github.com>
2024-11-06 03:35:09 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+# XMPP account credentials
+JID = "username@xmppdomain"  # Your XMPP JID
+PASSWORD = "password"        # Password for the XMPP account
+TOOL_COMMAND = ""            # Enter your execution command here (Eg: nmap -A -O)
